f16741403fd60bc6a8dc0c2bc818253bca0fc962
Kami <kamipinillar@gmail.com>
2022-12-01 10:28:35 -0500

    init

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Home } from './screens/Home'
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Home />
+    </SafeAreaView>
+  )
+}
---
+++ screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export function Home() {
+  return (
+    <View>
+      <Text>init</Text>
+    </View>
+  )
+}
+
